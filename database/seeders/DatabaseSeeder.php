be57951737dd824ac31fc95b51f86c60e9df5817
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-07-31 10:34:03 +0700

    feat: add database seeder for dummy users, posts, likes, and comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,46 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Like;
+use App\Models\Post;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(5)->create();
 
-        User::factory()->create([
+        $mainUser = User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('password'),
         ]);
+
+        $allUsers = $users->push($mainUser);
+
+        $posts = collect();
+        $allUsers->each(function ($user) use (&$posts) {
+            $userPosts = Post::factory(3)->create(['user_id' => $user->id]);
+            $posts = $posts->merge($userPosts);
+        });
+
+        $posts->each(function ($post) use ($allUsers) {
+            $allUsers->random(rand(0, $allUsers->count()))->each(function ($user) use ($post) {
+                Like::factory()->create([
+                    'user_id' => $user->id,
+                    'post_id' => $post->id,
+                ]);
+            });
+        });
+
+        $posts->each(function ($post) use ($allUsers) {
+            Comment::factory(rand(0, 4))->create([
+                'post_id' => $post->id,
+                'user_id' => $allUsers->random()->id,
+            ]);
+        });
     }
 }
