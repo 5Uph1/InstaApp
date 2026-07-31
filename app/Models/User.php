8d015d235c66db8dc89f9528e0bc05c5dc886880
Alif Sufi Muhsinin <alifsufim@gmail.com>
2026-07-31 10:29:55 +0700

    fix: add missing HasFactory trait to User and Like models

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
+    
     protected $fillable = [
         'name',
         'email',
